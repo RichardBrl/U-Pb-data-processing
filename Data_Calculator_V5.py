b21f377a03d3e4c3193303aad04e0fda43bebb20
RichardBrl <rbrauel@web.de>
2024-09-11 14:21:21 +0200

    Update Data_Calculator_V5.py

--- Data_Calculator_V5.py
+++ Data_Calculator_V5.py
@@ -9,3 +9,13 @@ import re
 
 input_file = r'./input_files/2024-05-25 Seq1 All csv.xlsb'
 output_file = r'./output_files/StatDat_.xlsx'
+
+#Gewünschte Einstellungen. Default-Einstellungen wurden vorgegeben
+skip_first_channel = input(f"Skip first channel if saturated? (yes/no) [default: yes]: ").strip().lower() or "yes"
+min_cps = int(input(f"Minimum CPS to calculate ACF [default: 500000]: ") or 500000)
+max_cps = int(input(f"Maximum CPS to calculate ACF for [default: 50000000]: ") or 50000000)
+
+#Sheet-Namen speichern, wird für die Output-Datei benötigt
+with open_workbook(input_file) as wb:
+    sheet_names = wb.sheets
+
